ece5e93006acff6917109d54b671a89e369580f4
Michael Dahl <michaeldahl7@gmail.com>
2024-02-01 11:20:39 -0800

    Setup postgres

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,13 +6,13 @@ export default function Home() {
       <h1>All Quizzes</h1>
       <ul>
         <li>
-          <a href="/quiz/1">Quiz 1</a>
+          <Link href="/quiz/1">Quiz 1</Link>
         </li>
         <li>
-          <a href="/quiz/2">Quiz 2</a>
+          <Link href="/quiz/2">Quiz 2</Link>
         </li>
         <li>
-          <a href="/quiz/3">Quiz 2</a>
+          <Link href="/quiz/3">Quiz 3</Link>
         </li>
       </ul>
     </section>
