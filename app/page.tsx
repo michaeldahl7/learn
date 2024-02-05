4f6e87237cd49db33057a8ad7cd3a883adb9ef5c
Michael Dahl <michaeldahl7@gmail.com>
2024-02-05 11:15:15 -0800

    Finished quizz logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,35 @@
 import Link from "next/link";
+import postgres from "postgres";
+import { Suspense } from "react";
+
+const sql = postgres(process.env.POSTGRES_URL!);
+
+type Quiz = {
+  quiz_id: number;
+  title: string;
+};
+
+async function Quizzes() {
+  const quizzes: Quiz[] = await sql`SELECT * FROM quizzes`;
+
+  return (
+    <ul>
+      {quizzes.map((quiz) => (
+        <li key={quiz.quiz_id}>
+          <Link href={`/quiz/${quiz.quiz_id}`}>{quiz.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <section>
-      <h1>All Quizzes</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1">Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2">Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3">Quiz 3</Link>
-        </li>
-      </ul>
+      <h1 className="font-2xl font-semibold text-blue-700">All Quizzes</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Quizzes />
+      </Suspense>
     </section>
   );
 }
